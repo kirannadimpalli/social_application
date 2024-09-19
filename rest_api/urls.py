f447c5164f36b2ea5b09babad6e434540fe5bed1
kiran <kirannadimpalli2000@gmail.com>
2024-09-19 16:37:26 +0530

    fix: added user api CRUD opertions

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_user
+from .views import get_user, create_user, user_action
 
 urlpatterns = [
-    path('users/', get_user, name='get_user')
+    path('users/', get_user, name='get_user'),
+    path('users/create_user', create_user, name='create_user'),
+    path('users/<int:pk>', user_action, name='user_action'),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -7,6 +7,36 @@ from .serializer import UserSerializers
 
 @api_view(['GET'])
 def get_user(request):
-    return Response(UserSerializers({'name': 'kiran', 'age': 24 }).data)
+    user = User.objects.all()
+    serializer = UserSerializers(user, many=True)
+    return Response(serializer.data)
 
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET','DELETE', 'PUT'])
+def user_action(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except user.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'GET':
+        serializer = UserSerializers(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializers(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
