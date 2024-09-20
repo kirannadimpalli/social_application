1cf81d5635204cd360f9cf69c9ec46d5900d182e
kiran <kirannadimpalli2000@gmail.com>
2024-09-20 19:17:19 +0530

    feat: added friends request and user search functionality

--- social_application/settings.py
+++ social_application/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_api',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
 ]
 
 REST_FRAMEWORK = {
@@ -66,7 +66,7 @@ CACHES = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7), 
     'ROTATE_REFRESH_TOKENS': True,        
     'BLACKLIST_AFTER_ROTATION': True,            
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'social_application.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'main_db',
+        'NAME': 'social_db',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
