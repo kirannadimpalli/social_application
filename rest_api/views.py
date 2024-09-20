8f20ca707644ad2f3752d99b84f8580b3784e5a5
kiran <kirannadimpalli2000@gmail.com>
2024-09-20 09:59:24 +0530

    test: added test cases for jwt authorization

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.response import Response
 from rest_framework import status
@@ -68,7 +69,7 @@ def login(request):
     return Response({'token': token.key, 'user': serializer.data})
 
 @api_view(['GET'])
-@authentication_classes([SessionAuthentication, TokenAuthentication])
+@authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
     return Response("passed!")
--- tests/test_view.py
+++ tests/test_view.py
@@ -3,6 +3,7 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_api.models import UserDetails
 
 @pytest.fixture
 def api_client():
@@ -31,3 +32,50 @@ def test_user_login(api_client, django_user_model):
     response = api_client.post(url, login_data, format='json')
     assert response.status_code == status.HTTP_200_OK
     assert 'token' in response.data
+
+@pytest.mark.django_db
+def test_fetch_user(api_client):
+    url = reverse('get_user')
+    UserDetails.objects.create(name='kiran', age=25)
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 1
+    assert response.json()[0]['name'] == 'kiran'
+
+@pytest.mark.django_db
+def test_create_user(api_client):
+    url = reverse('create_user')
+    user_data = {
+        'name': 'john',
+        'age': 34
+    }
+    response = api_client.post(url, user_data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()['name'] == 'john'
+    assert response.json()['age'] == 34
+
+
+@pytest.mark.django_db
+def test_authorization(api_client):
+    url = reverse('signup')
+    signup_data = { "username": "test", "password": "test1234", "email": "test@mail.com" }
+    response = api_client.post(url, signup_data, format='json')
+
+    url = reverse('get_token')
+    login_data = { "username": "test", "password": "test1234", "email": "test@mail.com" }
+    response = api_client.post(url, login_data, format='json')
+    access_token = response.json().get('access')
+    assert access_token is not None, "Access token was not retrieved."
+
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + access_token)
+    url = reverse('test_token')
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+
+@pytest.mark.django_db
+def test_fail_authorization(api_client):
+    access_token = 'dakjbasjbdlasjd'
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + access_token)
+    url = reverse('test_token')
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
